57d1614ca54530c50154b4ab64185526e9834247
bao71 <baoss712005@gmail.com>
2026-08-04 04:31:03 +0000

    update writer delta overwrite

--- src/core/writers.py
+++ src/core/writers.py
@@ -82,15 +82,9 @@ class DeltaWriter(BaseWriter):
                             .mode("overwrite")
                             .partitionBy(*partition_cols)
                             .option("path", self.config.get("path", f"s3a://warehouse/{db_name}/{table_name}"))
-                            .save()
+                            .saveAsTable(fqn_table_name)
         )
-        if first:
-            self.spark.sql(f"DROP TABLE IF EXISTS {db_name}.{table_name}")
-            self.spark.sql(f"""
-                CREATE TABLE IF NOT EXISTS {db_name}.{table_name}
-                USING delta
-                LOCATION '{self.config.get("path", f"s3a://warehouse/{db_name}/{table_name}")}'
-            """)
+        
 
 
     def append(self, df: DataFrame):
